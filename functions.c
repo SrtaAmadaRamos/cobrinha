3e1894bca7a7e15476421c57dccc2c4590846e45
Amanda <35702715+SrtaAmadaRamos@users.noreply.github.com>
2018-09-09 21:19:13 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -7,7 +7,8 @@ void limparMatriz(){
 }
 
 void imprimirMatriz(){
-  for(int i = 0; i < TAM; i++){ //colunas
+    printf("Resta %d movimentos\n", doce.vida);
+    for(int i = 0; i < TAM; i++){ //colunas
         printf("|");
         for(int j = 0; j < TAM; j++){               //barrinha no inicio e
             printf(" %c ", matriz[i][j]);           //fim de cada linha
@@ -33,11 +34,8 @@ void config(){
 	ponteiroNulo(minhoca.p);
 	
 	for(int i = 0; i < minhoca.tamanho; i++){
-		minhoca.p[i].x = 0;
-		minhoca.p[i].y = minhoca.tamanho - i - 1;
-
-		printf("(%d, %d))\n", 0, (minhoca.tamanho - i -1));
-
+        minhoca.p[i].x = minhoca.tamanho - i - 1;
+		minhoca.p[i].y = 0;
 	}
 
 	gerarDoce();
@@ -57,7 +55,7 @@ void gerarDoce(){
 		if(doce.posi.x == minhoca.p[i].x  &&  doce.posi.y == minhoca.p[i].y ){
 			doce.posi.x = rand()%10;
 			doce.posi.y = rand()%10;
-			i = 0;
+			i = -1;
 		}		
 	}
 
@@ -66,69 +64,96 @@ void gerarDoce(){
 }
 
 void colocaObjeto(){
+    matriz[ doce.posi.x ][ doce.posi.y ] = '*';
 	for(int i = 0; i < minhoca.tamanho; i++){
-		matriz[ minhoca.p[i].y ][ minhoca.p[i].x ] = '0';
-	}
-	matriz[ doce.posi.x ][ doce.posi.y ] = '*';
+		matriz[ minhoca.p[i].x ][ minhoca.p[i].y ] = '0';
+	}	
 }
 
 void movimentar(Posicao p){
-  	
 	minhoca.p = (Posicao *) realloc( minhoca.p, sizeof(Posicao) * minhoca.tamanho );
+	for( int i = minhoca.tamanho -1 ; i >= 0 ;i-- ){
+        if(i == 0){
+            minhoca.p[i] = p;
+        }else{
+            minhoca.p[i] = minhoca.p[i - 1] ;
+        }
+    }
 
-	for( int i =0; i < minhoca.tamanho; i++ )
+    for(int i = 1; i < minhoca.tamanho - 1; i++){
+        if(minhoca.p[0].x == minhoca.p[i].x  &&  minhoca.p[0].y == minhoca.p[i].y){
+            sair(2);
+        }
+    }
 
-	doce.vida--;
+    if( TAM < minhoca.p[0].x || minhoca.p[0].x < 0  ||  TAM < minhoca.p[0].y  ||  minhoca.p[0].y < 0 ){
+        sair(3);
+    }
 }
 
 void jogar(){
 	char op; 
+	Posicao posiCabeca = minhoca.p[0];;
+	limparTela();
+    int valida = 1; 
+    char letra;
+    op = 's';
 
-	Posicao posiCabeca;
-	
-    while(1){
-
-    	limparTela();
+    while(1){   
 
         imprimirMatriz();
-        op = getch();
+        sleep(1);
 
-        posiCabeca = minhoca.p[0];
+        if(kbhit()){
+            letra = getch();
 
+            if( letra == 'a' ||
+                letra == 'A' ||
+                letra == 's' ||
+                letra == 'S' ||
+                letra == 'd' ||
+                letra == 'D' ||
+                letra == 'w' ||
+                letra == 'W' ||
+                letra == 'q' ||
+                letra == 'Q'
+            ){
+                valida = 1;
+                op = letra ;
+            }else{
+                valida = 0;
+            }
+
+        }
+        
         switch(op){
         	case 'a':
         	case 'A':
-        		posiCabeca.x--;
+        		posiCabeca.y--;
+                doce.vida--;
         		break;
         	case 'w' :
         	case 'W' :
-        		posiCabeca.y--;
+        		posiCabeca.x--;
+                doce.vida--;
         		break;
 			case 'd' :
         	case 'D' :
-        		posiCabeca.x++;
+        		posiCabeca.y++;
+                doce.vida--;
         		break;
         	case 's' :
         	case 'S' :
         		posiCabeca.x++;
+                doce.vida--;
         		break;
         	case 'q' :
         	case 'Q' :
-        		sair();
+        		sair(4);
         		break;
         	default :
         		limparTela();
-                printf("Tecla inválida\n");
-                printf("(Tecla W) Para cima\n");
-                printf("(Tecla A) Para esquerda\n");
-                printf("(Tecla S) Para baixo\n");
-                printf("(Tecla D) Para direita\n");
-                printf("(Tecla Q) Para sair\n\n");
                 break;
-        }
-
-        if(doce.vida == 0){
-        	gerarDoce();
         }
 
         if(posiCabeca.x == doce.posi.x  &&  posiCabeca.y == doce.posi.y ){
@@ -136,23 +161,30 @@ void jogar(){
      		gerarDoce();
         }
 
+        movimentar(posiCabeca);
+
+        if(doce.vida == 0){
+            gerarDoce();
+        }            
+
         if(minhoca.tamanho == TAM_MAX ){
-        	printf("Fim de jogo, você atingiu o tamanho máximo da minhoca!  :D\n");
-        	sair();
-        }
+            sair(1);
+        }  
 
-        for(int i = 0;i < minhoca.tamanho; i++){
-        	if(posiCabeca.x == minhoca.p[i].x  &&  posiCabeca.y == minhoca.p[i].y){
-        		printf("Fim de jogo, a minhoca bateu no própio corpo! :(\n");
-        		sair();
-        	}
-        }
+        limparMatriz();
 
-        if( TAM < posiCabeca.x || posiCabeca.x < 0  ||  TAM < posiCabeca.x  ||  posiCabeca.x < 0 ){
-        	printf("Fim de jogo, você bateu nas extremidades do tabuleiro!  :(\n");
-        	sair();
-        }
+        colocaObjeto();
+        limparTela();
 
+        
+        if(!valida){
+            printf("Tecla inválida\n");
+            printf("(Tecla W) Para cima\n");
+            printf("(Tecla A) Para esquerda\n");
+            printf("(Tecla S) Para baixo\n");
+            printf("(Tecla D) Para direita\n");
+            printf("(Tecla Q) Para sair\n\n");
+        }
 
     }
 }
@@ -161,8 +193,15 @@ void limparTela(){
    system("@cls||clear");
 }
 
-void sair(){
+void sair(int op){
 
+    if(op == 1)
+        printf("Fim de jogo, você atingiu o tamanho máximo da minhoca!  :D\n");
+    if(op == 2)
+        printf("Fim de jogo, a minhoca bateu no própio corpo! :(\n");
+    if(op == 3)
+        printf("Fim de jogo, você bateu nas extremidades do tabuleiro!  :(\n");
+    
 	free(minhoca.p);
 	exit(1);
 }
