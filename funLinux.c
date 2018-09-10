025a843b0606c05579ac9faae39d97fbe1995354
Amanda <35702715+SrtaAmadaRamos@users.noreply.github.com>
2018-09-09 21:20:30 -0300

    Add files via upload

---
+++ funLinux.c
@@ -0,0 +1,37 @@
+int getch(void) {
+    struct termios velho, novo;
+    int x;
+
+    tcgetattr( STDIN_FILENO, &velho );
+    novo= velho;
+    novo.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &novo );
+    x= getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &velho );
+    return x;
+}
+
+int kbhit(void) {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF) {
+    	ungetc(ch, stdin);
+    	return 1;
+    }
+
+    return 0;
+}
---
+++ includes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+
+#ifndef _WIN32
+	#include <termios.h>
+	#include <unistd.h>
+	#include <fcntl.h>
+	#include "funLinux.c"
+#else
+	#include <conio.h>
+#endif
